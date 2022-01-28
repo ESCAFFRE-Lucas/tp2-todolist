8b9fdb41eac3887a2f602a767364f7447a7ec4ad
LESCAFFRE <lucas.escaffre@ynov.com>
2022-01-28 23:01:52 +0100

    copy list from todo to finish list but with some errors

--- main.js
+++ main.js
@@ -3,22 +3,36 @@ function addElement() {
     const inputValue = document.getElementById("todo-input").value;
     const text = document.createTextNode(inputValue);
     const unordered = document.getElementById("ul-list");
+    const finish = document.getElementById("ul-finish")
 
+    list.className = "finish"
     list.appendChild(text);
     document.getElementById("ul-list").appendChild(list);
     document.getElementById("todo-input").value = "";
 
     const span = document.createElement("SPAN");
     const txt = document.createTextNode("\uf061");
-    span.className = "close";
+    span.className = "next";
     span.appendChild(txt);
     list.appendChild(span);
 
-    const close = document.getElementsByClassName("close");
+    const next = document.getElementsByClassName("next");
 
-    for (let i = 0; i < close.length; i++) {
-        close[i].onclick = function () {
+    for (let i = 0; i < next.length; i++) {
+        next[i].onclick = function () {
+            const copy = list.cloneNode(false)
+            const copyTest = text.cloneNode(false)
+
             unordered.removeChild(list);
+            finish.append(copy, copyTest);
+
+            const finals = document.createElement("SPAN");
+            const cross = document.createTextNode("\uf00d");
+
+
+            finals.className = "close";
+            finals.appendChild(cross);
+            list.appendChild(finals);
         }
     }
 }
