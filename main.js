e27986bf2fdb5202b25e65c5cb36ac222a7adfc5
LESCAFFRE <lucas.escaffre@ynov.com>
2022-01-28 12:27:42 +0100

    todo list js advancement, close button finis and addeventlistener too

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 const button = document.getElementById("add-todo")
-const todo = document.getElementById("todo-list")
 
 function addElement() {
-    const newDiv = document.createElement("div");
-    const newContent = document.createTextNode('yo');
-    newDiv.appendChild(newContent);
-    todo.insertBefore(newDiv, button)
+    const list = document.createElement("li");
+    const inputValue = document.getElementById("todo-input").value;
+    const text = document.createTextNode(inputValue);
+    list.appendChild(text);
+    document.getElementById("ul-list").appendChild(list);
+    document.getElementById("todo-input").value = "";
+
+    const span = document.createElement("SPAN");
+    const txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    list.appendChild(span);
+
+    const close = document.getElementsByClassName("close");
+
+    for (let i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            const div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
 }
 
 button.addEventListener("click", addElement)
